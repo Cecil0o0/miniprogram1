4d4d67de925ae9ed4a3593a3d56f9c4d3d7caece
清风 <jiegithub@gmail.com>
2018-08-31 01:29:48 +0800

    feat: first commit

---
+++ src/pages/info-video-edit/index.js
@@ -0,0 +1,29 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import './index.styl'
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '视频编辑'
+  }
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Self Info</Text>
+      </View>
+    )
+  }
+}
+
