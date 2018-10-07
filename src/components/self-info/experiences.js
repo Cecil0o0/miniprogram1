8c919a6f51f21c13a1cf3c4d4839651450bc3232
清风 <jiegithub@gmail.com>
2018-10-07 22:27:18 +0800

    feat: 细节优化

---
+++ src/components/self-info/experiences.js
@@ -0,0 +1,66 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Image } from '@tarojs/components'
+import ListPng from '../../images/list.png'
+import ActionPng from '../../images/action.png'
+import PersonOPng from '../../images/person_o.png'
+import PeoplePng from '../../images/people.png'
+import filters from '../../lib/filter'
+import './experiences.styl'
+
+export default class Experiences extends Component {
+  defaultProps = {
+    info: [
+      {
+        year: 2013,
+        name: '非诚勿扰',
+        // 1: 电影
+        type: 1,
+        director: '冯小刚',
+        character: '王嘉涵',
+        coActor: ['葛优', '舒淇', '郑恺']
+      }
+    ]
+  }
+  render() {
+    const exps = this.props.info || this.props.defaultProps.info
+    return (
+      <View className="experiences-wrapper">
+        {exps.map((exp, idx) => {
+          return (
+            <View key={idx} className="item">
+              <View className="name">{exp.name}</View>
+              <View className="content">
+                <View>
+                  <Image src={ListPng} />
+                  类型
+                </View>
+                <View>{filters.expType(exp.type)}</View>
+              </View>
+              <View className="content">
+                <View>
+                  <Image src={ActionPng} />
+                  导演
+                </View>
+                <View>{exp.director}</View>
+              </View>
+              <View className="content">
+                <View>
+                  <Image src={PersonOPng} />
+                  饰演角色
+                </View>
+                <View>{exp.character}</View>
+              </View>
+              <View className="content">
+                <View>
+                  <Image src={PeoplePng} />
+                  合作演员
+                </View>
+                <View>{exp.coActor.join(' ')}</View>
+              </View>
+            </View>
+          )
+        })}
+      </View>
+    )
+  }
+}
--- src/components/self-info/index.js
+++ src/components/self-info/index.js
@@ -1,5 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Image, Video } from '@tarojs/components'
+import cn from 'classnames'
 import './index.styl'
 import PersonPng from '../../images/person.png'
 import CakePng from '../../images/cake.png'
@@ -12,6 +13,9 @@ import JobPng from '../../images/job.png'
 import CrownPng from '../../images/crown.png'
 import VideoPng from '../../images/black_video.png'
 import ExpPng from '../../images/yanyijingli.png'
+import CaretRightPng from '../../images/caret_right.png'
+import PointPng from '../../images/point.png'
+import Experiences from './experiences'
 
 export default class SelfInfo extends Component {
   defaultProps = {
@@ -28,17 +32,60 @@ export default class SelfInfo extends Component {
       sex: 0,
       popularity: 1258,
       subscribe: 225874,
-      intro:
-        '坚持不一定会胜利，放弃不一定是认输，人生有很多时候需要的不仅仅是执着，更是回眸一笑的洒脱，加油！',
+      intro: '坚持不一定会胜利，放弃不一定是认输，人生有很多时候需要的不仅仅是执着，更是回眸一笑的洒脱，加油！',
       bwh: [45, 48, 52],
       city: '北京市',
       school: '北京电影学院',
       exp: '三年平面模特',
-      specialities: ['唱歌', '跳舞', '书法']
+      specialities: ['唱歌', '跳舞', '书法'],
+      experiences: [
+        {
+          year: 2013,
+          name: '非诚勿扰',
+          // 1: 电影
+          type: 1,
+          director: '冯小刚',
+          character: '王嘉涵',
+          coActor: ['葛优', '舒淇', '郑恺']
+        },
+        {
+          year: 2013,
+          name: '非诚勿扰',
+          // 1: 电影
+          type: 1,
+          director: '冯小刚',
+          character: '王嘉涵',
+          coActor: ['葛优', '舒淇', '郑恺']
+        },
+        {
+          year: 2014,
+          name: '非诚勿扰',
+          // 1: 电影
+          type: 1,
+          director: '冯小刚',
+          character: '王嘉涵',
+          coActor: ['葛优', '舒淇', '郑恺']
+        }
+      ],
+      other: `2013年，范冰冰蝉联2014福布斯中国名人榜第一位，5月23日，布莱恩·辛格导演，休·杰克曼；詹姆斯·麦卡沃伊；迈克尔·法斯宾德主演的电影《X战警：逆转未来》全球同步上映，范冰冰在其中饰演“闪烁女Blink”一角。`
     }
   }
+  state = {
+    selectedExp: 0
+  }
+  handleClick(idx) {
+    this.setState({
+      selectedExp: idx === this.state.selectedExp ? -1 : idx
+    })
+  }
   render() {
-    const info = this.props.info || this.defaultProps.info
+    const info = this.defaultProps.info || this.props.info || this.defaultProps.info
+    const years = {}
+    info.experiences &&
+      info.experiences.forEach(item => {
+        if (!years[item.year]) years[item.year] = []
+        years[item.year].push(item)
+      })
     return (
       <View className="self-info">
         <View className="basic wrapper">
@@ -117,7 +164,33 @@ export default class SelfInfo extends Component {
             <Image src={ExpPng} className="crown" />
             演艺经历
           </View>
-          <View className="inner-wrapper">
+          <View className="inner-wrapper accordion-wrapper">
+            {Object.keys(years).map((year, idx) => {
+              let everyYears = years[year]
+              return (
+                <View
+                  className={cn('accordion-item', {
+                    active: this.state.selectedExp === idx
+                  })}
+                  onClick={this.handleClick.bind(this, idx)}
+                  key={idx}
+                >
+                  <View className="icon">
+                    <Image src={PointPng} />
+                  </View>
+                  <View className="content">
+                    <View className="header">
+                      {year}年 <Image src={CaretRightPng} className="caret" />
+                    </View>
+                    <View className="body">
+                      <Experiences info={everyYears || []} />
+                      <View className="other-title">其他</View>
+                      <View className="other">{info.other}</View>
+                    </View>
+                  </View>
+                </View>
+              )
+            })}
           </View>
         </View>
       </View>
